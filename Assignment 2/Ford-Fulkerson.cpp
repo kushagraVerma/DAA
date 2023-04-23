10d00ca94f193159b10bc37b49a9f16e9ad708e3
SriramB2002 <f20200002@hyderabad.bits-pilani.ac.in>
2023-04-23 13:10:23 +0530

    Added code for finding s-t cut

--- Assignment 2/Ford-Fulkerson.cpp
+++ Assignment 2/Ford-Fulkerson.cpp
@@ -33,12 +33,36 @@ vector<int> findPath(int n, int source, int sink, vector<vector<int>> &residualG
     reverse(path.begin(), path.end());
     return path;
 }
+
+vector<int> findReachableVertices(int n, int source, int sink, vector<vector<int>> &residualGraph) {
+    vector<int> visited(n);
+    queue<int> q;
+    q.push(source);
+    visited[source] = true;
+
+    while (!q.empty())
+    {
+        int k = q.front();
+        q.pop();
+        visited[k] = 1;
+        for (int i = 0; i < residualGraph[k].size(); i++)
+        {
+            if (residualGraph[k][i] && !visited[i])
+            {
+                visited[i] = true;
+                q.push(i);
+            }
+        }
+    }
+    return visited;
+}
     
 int32_t main() {
     int n, m;
     cin >> n >> m;
     vector<vector<int>> originalGraph(n, vector<int>(n));
     vector<vector<int>> residualGraph(n, vector<int>(n));
+    vector<vector<int>> flowGraph(n, vector<int>(n));
     
     for (int i = 0; i < m; i++)
     {
@@ -69,8 +93,35 @@ int32_t main() {
         for (int i = 0; i < path.size()-1; i++)
         {
             residualGraph[path[i]][path[i+1]] -= bottleneck;
-            residualGraph[path[i+1]][path[i]] += bottleneck; 
+            residualGraph[path[i+1]][path[i]] += bottleneck;
+            flowGraph[path[i]][path[i+1]] += bottleneck;
         }
     }
     cout << flow << endl;
+    for (int i = 0; i < flowGraph.size(); i++)
+    {
+        for (int j = 0; j < flowGraph[i].size(); j++)
+        {
+            cout << flowGraph[i][j];
+            if (j != flowGraph.size() - 1) cout << ",";
+        }
+        cout << endl;
+    }
+    
+    // Finding the min s-t cut for the graph
+    vector<pair<int,int>> cutEdges;
+    vector<int> visited = findReachableVertices(n, source, sink, residualGraph);
+    for (int i = 0; i < visited.size(); i++)
+    {
+        for (int j = i; j < visited.size(); j++)
+        {
+            if (visited[i] && !visited[j] && originalGraph[i][j]) cutEdges.push_back({i, j});
+        }
+    }
+    cout << cutEdges.size() << endl;
+    for (int i = 0; i < cutEdges.size(); i++)
+    {
+        cout << cutEdges[i].first << "-" << cutEdges[i].second << endl;
+    }
+    
 }
