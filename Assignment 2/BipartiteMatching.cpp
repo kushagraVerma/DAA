9b59923aee26e5170e8bfba37be915a05875f9f5
SriramB2002 <f20200002@hyderabad.bits-pilani.ac.in>
2023-04-29 01:31:51 +0530

    Added documentation and genreated Doxyfile

--- Assignment 2/BipartiteMatching.cpp
+++ Assignment 2/BipartiteMatching.cpp
@@ -2,6 +2,12 @@
 #define int long long int
 using namespace std;
 
+/**
+ * Partitions a bipartite graph into two vertex sets 
+ * @param n number of vertices
+ * @param graph a graph in adjacency matrix format
+ * @return a vector that gives the partition set number of every vertex 
+*/
 vector<int> partitionGraph(int n, vector<vector<int>> &graph) {
     vector<int> partition(n);
     vector<int> visited(n);
@@ -27,6 +33,14 @@ vector<int> partitionGraph(int n, vector<vector<int>> &graph) {
     return partition;
 }
 
+/**
+ * Finds a path between a source and a sink in a residual graph
+ * @param n number of vertices
+ * @param source source vertex number
+ * @param sink sink vertex number
+ * @param residualGraph a residual graph in adjacency matrix format
+ * @return a vector containing vertices along the source-sink path
+*/
 vector<int> findPath(int n, int source, int sink, vector<vector<int>> &residualGraph) {
     vector<int> parent(n, -1);
     vector<int> visited(n);
@@ -60,9 +74,9 @@ vector<int> findPath(int n, int source, int sink, vector<vector<int>> &residualG
 }
 
 int32_t main() {
-    int n,m;
+    int n,m; // Number of vertices and edges
     cin >> n >> m;
-    vector<vector<int>> adjList(n, vector<int>(n));
+    vector<vector<int>> adjList(n, vector<int>(n)); // Graph stored in adjacency list format
     for (int i = 0; i < m; i++)
     {
         int a,b;
@@ -73,21 +87,21 @@ int32_t main() {
         adjList[b][a] = 1;
     }
     vector<int> partition = partitionGraph(n, adjList);
-    vector<int> partition1, partition2;
+    vector<int> partition1, partition2; // Splitting the vertices into two sets
     for (int i = 0; i < partition.size(); i++)
     {
         if (partition[i] == 1) partition1.push_back(i + 1);
         else if (partition[i] == 2) partition2.push_back(i + 1);
     }
 
-    vector<vector<int>> directedGraph(n+2, vector<int>(n+2));
+    vector<vector<int>> residualGraph(n+2, vector<int>(n+2)); // Creating a residual directed graph with a temporary source and sink to run Ford-Fulkerson algorithm
     for (int i = 0; i < partition1.size(); i++)
     {
-        directedGraph[0][partition1[i]] = 1;
+        residualGraph[0][partition1[i]] = 1;
     }
     for (int i = 0; i < partition2.size(); i++)
     {
-        directedGraph[partition2[i]][n+1] = 1;
+        residualGraph[partition2[i]][n+1] = 1;
     }
     for (int i = 0; i < partition1.size(); i++)
     {
@@ -95,36 +109,41 @@ int32_t main() {
         {
             int vertex1 = partition1[i] - 1;
             int vertex2 = partition2[j] - 1;
-            if (adjList[vertex1][vertex2]) directedGraph[vertex1 + 1][vertex2 + 1] = 1;
+            if (adjList[vertex1][vertex2]) residualGraph[vertex1 + 1][vertex2 + 1] = 1;
         }
     }
-    vector<vector<int>> flowGraph(n + 2, vector<int>(n + 2));
+    vector<vector<int>> flowGraph(n + 2, vector<int>(n + 2)); // Graph to denote flow through the edges
     
     int source = 0, sink = n + 1;
     while (1)
     {
-        vector<int> path = findPath(n + 2, source, sink, directedGraph);
+        vector<int> path = findPath(n + 2, source, sink, residualGraph);
         if (path.size() < 2) break;
         for (int i = 0; i < path.size() - 1; i++)
         {
-            directedGraph[path[i]][path[i + 1]] -= 1;
-            directedGraph[path[i + 1]][path[i]] += 1;
+            // Updating forward and backward edges in residual graph
+            residualGraph[path[i]][path[i + 1]] -= 1;
+            residualGraph[path[i + 1]][path[i]] += 1;
+
+            // Updating the flow through the edges in the flow graph
             flowGraph[path[i]][path[i + 1]] += 1;
         }
     }
     for (int i = 0; i < flowGraph.size(); i++)
     {
+        // Resetting the flows through the temporary source and sink back to 0, since we do not need them after Ford-Fulkerson
         flowGraph[0][i] = 0;
         flowGraph[i][n + 1] = 0;
     }
     
+    cout << "The edges part of the maximum bipartite matching are:" << endl;
     for (int i = 0; i < partition2.size(); i++)
     {
         for (int j = 0; j < partition1.size(); j++)
         {
-            if (directedGraph[partition2[i]][partition1[j]])
+            if (residualGraph[partition2[i]][partition1[j]])
             {
-                cout << partition2[i] << "-" << partition1[j] << endl;
+                cout << partition1[j] << "-" << partition2[i] << endl;
             }
         }
         
--- Assignment 2/Ford-Fulkerson.cpp
+++ Assignment 2/Ford-Fulkerson.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
 #define int long long int
 using namespace std;
-    
+
+/**
+ * Finds a path from a given source to a sink
+ * @param n number of vertices in the residual graph
+ * @param source source vertex number
+ * @param sink sink vertex number
+ * @param residualGraph A residual graph in adjacency matrix formal
+ * @return a vector containing the vertex numbers of a path from the given source to sink
+*/
 vector<int> findPath(int n, int source, int sink, vector<vector<int>> &residualGraph) {
     vector<int> parent(n, -1);
     vector<int> visited(n);
@@ -34,7 +42,14 @@ vector<int> findPath(int n, int source, int sink, vector<vector<int>> &residualG
     return path;
 }
 
-vector<int> findReachableVertices(int n, int source, int sink, vector<vector<int>> &residualGraph) {
+/**
+ * Finds all reachable vertices from a particular source in a residual graph
+ * @param n number of vertices in the residual graph
+ * @param source source vertex number
+ * @param residualGraph a residual graph in adjacency matrix format
+ * @return a vector representing the reachability of every vertex from the given source.
+*/
+vector<int> findReachableVertices(int n, int source, vector<vector<int>> &residualGraph) {
     vector<int> visited(n);
     queue<int> q;
     q.push(source);
@@ -58,11 +73,11 @@ vector<int> findReachableVertices(int n, int source, int sink, vector<vector<int
 }
     
 int32_t main() {
-    int n, m;
+    int n, m; //> Number of vertices and edges in the original graph
     cin >> n >> m;
-    vector<vector<int>> originalGraph(n, vector<int>(n));
-    vector<vector<int>> residualGraph(n, vector<int>(n));
-    vector<vector<int>> flowGraph(n, vector<int>(n));
+    vector<vector<int>> originalGraph(n, vector<int>(n)); // The original graph represented in adjacency matrix format
+    vector<vector<int>> residualGraph(n, vector<int>(n)); // Initializing residual graph with flows initially set as 0 for all forward edges
+    vector<vector<int>> flowGraph(n, vector<int>(n)); // FILL THIS
     
     for (int i = 0; i < m; i++)
     {
@@ -74,7 +89,7 @@ int32_t main() {
         residualGraph[a][b] += val;
     }
     
-    int source, sink;
+    int source, sink; // Source and sink vertices
     cin >> source >> sink;
     source--;
     sink--;
@@ -83,7 +98,7 @@ int32_t main() {
     while (1)
     {
         vector<int> path = findPath(n, source, sink, residualGraph);
-        if (path.size() < 2) break;
+        if (path.size() < 2) break; // No path found
         int bottleneck = 1e18;
         for (int i = 0; i < path.size()-1; i++)
         {
@@ -92,36 +107,33 @@ int32_t main() {
         flow += bottleneck;
         for (int i = 0; i < path.size()-1; i++)
         {
+            // Updating the forward and backward edges in the residual graph
             residualGraph[path[i]][path[i+1]] -= bottleneck;
             residualGraph[path[i+1]][path[i]] += bottleneck;
+
+            // Adding the flow for corresponding edges in the flow graph
             flowGraph[path[i]][path[i+1]] += bottleneck;
         }
     }
-    cout << flow << endl;
-    for (int i = 0; i < flowGraph.size(); i++)
-    {
-        for (int j = 0; j < flowGraph[i].size(); j++)
-        {
-            cout << flowGraph[i][j];
-            if (j != flowGraph.size() - 1) cout << ",";
-        }
-        cout << endl;
-    }
+    cout << "The max flow of the network: " << flow << endl;
     
     // Finding the min s-t cut for the graph
-    vector<pair<int,int>> cutEdges;
-    vector<int> visited = findReachableVertices(n, source, sink, residualGraph);
+    vector<pair<int,int>> cutEdges; // Vector to store the min s-t cut edges of the graph
+    vector<int> visited = findReachableVertices(n, source, residualGraph);
     for (int i = 0; i < visited.size(); i++)
     {
         for (int j = i; j < visited.size(); j++)
         {
+            // Finding the vertices that are not reachable in the residual graph, but have a forward edge in the original graph
+            // This means that the capacity has been exhausted by the flow
             if (visited[i] && !visited[j] && originalGraph[i][j]) cutEdges.push_back({i, j});
         }
     }
-    cout << cutEdges.size() << endl;
+    // cout << "No. of edges in the min s-t cut: " << cutEdges.size() << endl;
+    cout << "Edges in the min s-t cut:" << endl;
     for (int i = 0; i < cutEdges.size(); i++)
     {
-        cout << cutEdges[i].first << "-" << cutEdges[i].second << endl;
+        cout << cutEdges[i].first + 1 << "-" << cutEdges[i].second + 1 << endl;
     }
-    
+    return 0;
 }
